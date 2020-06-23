4ad8d8c6de55e0fb1d4c28fe0b44670277fd827b
grem <love@grem.ru>
2020-06-23 12:26:36 +0700

    rows with csv

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,14 +1,28 @@
 # pcost.py
 #
 # Exercise 1.27
+import csv
+import sys
 
-total = 0.0
+def portfolio_cost(filename):
+    total = 0.0
 
-with open('Data/portfolio.csv','rt') as f:
-    next(f)
-    for line in f:
-        row = line.strip().split(',')
-        total += int(row[1]) * float(row[2])
+    with open(filename,'rt') as f:
+        rows = csv.reader(f)
+        next(f) #headers
+        for row in rows:
+            #row = line.strip().split(',')
+            try:
+                total += int(row[1]) * float(row[2])
+            except ValueError:
+                print("Couldn't parse", line)
+    return total
 
-print('Total cost', total)
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = 'Data/portfolio.csv'
+
+cost = portfolio_cost(filename)
+print('Total cost', cost)
 
